5ecbd613b74cf2c73dfa212648089e9adc35d493
fenng <xqjia123@gmail.com>
2023-06-28 08:07:32 +0800

    word count parallel thread

--- src/main/java/coding/buffer/WordCount.java
+++ src/main/java/coding/buffer/WordCount.java
@@ -3,13 +3,21 @@ package coding.buffer;
 import org.junit.Test;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.channels.FileChannel;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.StringTokenizer;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.Future;
 
 /**
  * @Author: Jaa
@@ -19,6 +27,71 @@ import java.util.concurrent.ForkJoinPool;
 public class WordCount {
 
     final ForkJoinPool pool = ForkJoinPool.commonPool();
+
+    class CountTask implements Callable<HashMap<String, Integer>> {
+        private final long start;
+        private final long end;
+        private final String fileName;
+
+        public CountTask(String fileName, long start, long end) {
+            this.fileName = fileName;
+            this.start = start;
+            this.end = end;
+        }
+
+        @Override
+        public HashMap<String, Integer> call() throws Exception {
+            var channel = new RandomAccessFile(this.fileName, "rw").getChannel();
+            // [start, end] -> Memory
+            // Device -> Kernel Space -> UserSpace(buffer) -> Thread
+            var mbuf = channel.map(
+                    FileChannel.MapMode.READ_ONLY,
+                    this.start,
+                    this.end - start
+            );
+            var str = StandardCharsets.US_ASCII.decode(mbuf).toString();
+            return countByString(str);
+        }
+    }
+
+    public void run(String fileName, long chunkSize) throws ExecutionException, InterruptedException {
+        var file = new File(fileName);
+        var fileSize = file.length();
+
+        long position = 0;
+
+        var startTime = System.currentTimeMillis();
+        var tasks = new ArrayList<Future<HashMap<String, Integer>>>();
+        while (position < fileSize) {
+            var next = Math.min(position + chunkSize, fileSize);
+            var task = new CountTask(fileName, position, next);
+            position = next;
+            ForkJoinTask<HashMap<String, Integer>> future = pool.submit(task);
+            tasks.add(future);
+        }
+        System.out.format("split to %d tasks\n", tasks.size());
+
+        var totalMap = new HashMap<String, Integer>();
+        for(var future: tasks) {
+            var map = future.get();
+            for(var entry : map.entrySet()) {
+                incKey(entry.getKey(), totalMap, entry.getValue());
+            }
+        }
+
+        System.out.println("time:" + (System.currentTimeMillis() - startTime) + "ms");
+        System.out.println("total:" + totalMap.size());
+
+        System.out.println(totalMap.get("ababb"));
+    }
+
+    @Test
+    public void count() throws ExecutionException, InterruptedException {
+        var counter = new WordCount();
+        System.out.println("processors:" + Runtime.getRuntime().availableProcessors());
+        counter.run("word", 1024*1024*20);
+    }
+
 
     @Test
     public void conpare_with_single() throws IOException {
